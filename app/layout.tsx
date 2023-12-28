1cc55dbb43c4e37214ec2c577918e7cd23084e09
minho <dnjsalsgh123@gmail.com>
2023-12-29 06:56:19 +0900

    update: move the header component to the main layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/thema-provider";
+import { NavigationHeader } from "@/components/navigation/navigation-header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NavigationHeader />
           {children}
         </ThemeProvider>
       </body>
