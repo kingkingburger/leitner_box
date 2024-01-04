d499039759ba01ef92517af80bcc08aa4c58cc65
wmh <wmh@uvc.co.kr>
2024-01-04 11:41:47 +0900

    update #3: setting border in memory-card

--- components/memory-card/create-memory-card.tsx
+++ components/memory-card/create-memory-card.tsx
@@ -9,8 +9,8 @@ import {Textarea} from "@/components/ui/textarea";
 export function CreateMemoryCard() {
 
   return (
-    <div className="flex  justify-center min-h-screen ">
-      <Card className="w-3/4">
+    <div className="flex justify-center ">
+      <Card className="w-3/4 border-4">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl">카드 만들기</CardTitle>
         </CardHeader>
@@ -20,12 +20,12 @@ export function CreateMemoryCard() {
             </div>
           <div className="grid gap-2">
             <Label className="p-1">제목</Label>
-            <Input id="text" type="text" placeholder="제목을 설정해주세요" />
+            <Input id="text" type="text" placeholder="제목을 설정해주세요" className="border-4"/>
           </div>
 
-          <div className="grid w-full gap-1.5 ">
+          <div className="grid w-full gap-1.5">
             <Label htmlFor="message">내용</Label>
-            <Textarea placeholder="내용을 작성해주세요" id="message" className="h-80"/>
+            <Textarea placeholder="내용을 작성해주세요" id="message" className="border-4 h-80"/>
           </div>
 
         </CardContent>
