3999d18c44ec26e487f81cf0d47c72c152a0af85
wmh <wmh@uvc.co.kr>
2024-01-04 11:20:19 +0900

    update #3: setting textarea memory-card

--- components/memory-card/create-memory-card.tsx
+++ components/memory-card/create-memory-card.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle,} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {Textarea} from "@/components/ui/textarea";
 
 export function CreateMemoryCard() {
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Card>
+    <div className="flex  justify-center min-h-screen ">
+      <Card className="w-3/4">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl">카드 만들기</CardTitle>
         </CardHeader>
@@ -23,15 +19,17 @@ export function CreateMemoryCard() {
               <span className="w-full border"></span>
             </div>
           <div className="grid gap-2">
-            <Label>제목</Label>
+            <Label className="p-1">제목</Label>
             <Input id="text" type="text" placeholder="제목을 설정해주세요" />
           </div>
-          <div className="grid gap-2">
-            <Label>내용</Label>
-            <Input id="text" type="text" placeholder="내용을 설정해주세요" />
+
+          <div className="grid w-full gap-1.5 ">
+            <Label htmlFor="message">내용</Label>
+            <Textarea placeholder="내용을 작성해주세요" id="message" className="h-80"/>
           </div>
+
         </CardContent>
-        <CardFooter>
+        <CardFooter className="flex justify-end">
           <Button>카드 생성</Button>
         </CardFooter>
       </Card>
