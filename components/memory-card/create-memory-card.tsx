b24addfdcd40ab4f93db4d4ad912ac46c4640dc8
kingkingburger <dnjsalsgh123@gmail.com>
2024-01-06 09:33:48 +0900

    update #3: git responsibility test

--- components/memory-card/create-memory-card.tsx
+++ components/memory-card/create-memory-card.tsx
@@ -43,7 +43,7 @@ export function CreateMemoryCard() {
   });
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log("git reponseability test");
+    console.log("git reponseability test2");
     console.log(values);
   };
   return (
