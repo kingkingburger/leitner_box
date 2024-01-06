d7c6c6aaefff3080f2a5ef0cd454635239201a08
minho <dnjsalsgh123@gmail.com>
2024-01-07 08:41:47 +0900

    update #2: change replace card tag to form tag

--- components/memory-card/create-memory-card.tsx
+++ components/memory-card/create-memory-card.tsx
@@ -47,51 +47,49 @@ export function CreateMemoryCard() {
   };
   return (
     <div className="flex justify-center">
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="title"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>제목</FormLabel>
-                <FormControl>
-                  <Input placeholder="제목을 입력해주세요" {...form} />
-                </FormControl>
-                <FormDescription>제목을 입력해주세요</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
-
       <Card className="w-3/4 border-4">
         <CardHeader className="space-y-1">
           <CardTitle className="text-2xl">카드 만들기</CardTitle>
         </CardHeader>
         <CardContent className="grid gap-4">
-          <div className="inset-0 flex items-center">
-            <span className="w-full border"></span>
-          </div>
-          <div className="grid gap-2">
-            <Label className="p-1">제목</Label>
-            <Input
-              id="text"
-              type="text"
-              placeholder="제목을 설정해주세요"
-              className="border-4"
-            />
-          </div>
-
-          <div className="grid w-full gap-1.5">
-            <Label htmlFor="message">내용</Label>
-            <Textarea
-              placeholder="내용을 작성해주세요"
-              id="message"
-              className="border-4 h-80"
-            />
-          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>제목</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="제목을 입력해주세요"
+                        {...form}
+                        className="border-4"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="content"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>내용</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="내용을 작성해주세요"
+                        className="border-4 h-80"
+                        {...form}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </CardContent>
         <CardFooter className="flex justify-end">
           <Button>카드 생성</Button>
